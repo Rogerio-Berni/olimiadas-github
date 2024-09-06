74fef3194a22a9586dbc93c67b80feed82ac8e9e
Rogerio Berni <44068613+Rogerio-Berni@users.noreply.github.com>
2024-09-06 17:45:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-// function pesquisar() {
-//     //console.log("clicou");
-//     let section = document.getElementById("resultados-pesquisas")
-
-//     let resultados = ""
-    
-//     for(let dado of dados) {
-//         resultados += `
-//              <div class="item-resultado">
-//                 <h2>
-//                     <a href="#" target="_blank">${dado.titulo}</a>
-//                 </h2>
-//                 <p class="descricao-meta">${dado.descricao}</p>
-//                 <a href=${dado.link} target="_blank">Mais inforações</a>
-//             </div>
-//     `
-//     }
-    
-//     section.innerHTML = resultados
-// }
-
-// //console.log(dados);
-
-
 function pesquisar() {
     // Esta função é responsável por realizar uma pesquisa e exibir os resultados na página.
     // Ela busca os dados da pesquisa em um array chamado "dados" e cria elementos HTML
@@ -32,27 +8,46 @@ function pesquisar() {
   
     // Obtém a seção HTML onde os resultados serão exibidos.
     let section = document.getElementById("resultados-pesquisas");
-  
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+      section.innerHTML = "<p>Nada foi encontrado</p>"
+      return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+   
     // Inicializa uma string vazia para armazenar os resultados da pesquisa formatados em HTML.
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
   
     // Itera sobre cada item de dados da pesquisa.
     for (let dado of dados) {
+       titulo = dado.titulo.toLowerCase()
+       descricao = dado.descricao.toLowerCase()
+       tags = dado.tags.toLowerCase()
       // Cria uma nova div para cada resultado, formatando-a com as informações do dado atual.
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>
-      `;
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        // cria um novo elemento
+        resultados += `
+          <div class="item-resultado">
+            <h2>
+              <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+        `;
     }
-  
-    // Atribui o conteúdo HTML gerado à seção de resultados.
-    section.innerHTML = resultados;
+  }
+
+  if (!resultados) {
+     resultados = "<p>Nada foi encontrado. Você precisa digitar o nome do atleta ou esporte</p>"
   }
   
-  // Descomenta a linha abaixo para verificar o conteúdo do array "dados".
-  // console.log(dados);
+  section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo : "Rayssa Leal",
         descricao : "Rayssa Leal, a Fadinha, é uma das maiores promessas do skate mundial. Com apenas [idade], ela já conquistou diversos títulos e se tornou um ícone para a nova geração de skatistas. Sua habilidade e carisma a tornaram uma das atletas mais populares do Brasil.",
-        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags : "skate fada sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma jovem promessa do judô brasileiro. Com apenas [idade], ela já demonstra grande talento e potencial para se tornar uma das maiores judocas do país. Sua dedicação e técnica a destacam no cenário nacional.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"            
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",  
+        tags : "judô judoca judoka"          
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração do país com suas performances incríveis. Com [idade], ela já coleciona diversas medalhas em competições nacionais e internacionais. Sua força e determinação a tornaram um exemplo para muitos.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags : "ginástica ginastica ouro"
     }    
 ];
